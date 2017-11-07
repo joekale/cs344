ae360a2e2b8967d44897c2d940acb857ac51478a
Joe Kale <jkale@autonomoustuff.com>
2017-11-06 21:14:06 -0600

    works with larger block size. also altered Makefile

--- Problem Sets/Problem Set 1/student_func.cu
+++ Problem Sets/Problem Set 1/student_func.cu
@@ -50,6 +50,21 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
   //First create a mapping from the 2D block and grid locations
   //to an absolute 2D location in the image, then use that to
   //calculate a 1D offset
+
+  int x = threadIdx.x + (blockIdx.x * blockDim.x);
+  int y = threadIdx.y + (blockIdx.y * blockDim.y);
+
+  if(x > numCols || y > numRows)
+  {
+    return;
+  }
+        
+  int offset = (numCols * y ) + x;
+       
+  uchar4 rgbPx = rgbaImage[offset];
+  unsigned char grey = 0.299f * rgbPx.x + 0.587f * rgbPx.y + 0.114f * rgbPx.z;
+
+  greyImage[offset] = grey;
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -57,8 +72,10 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
-  const dim3 gridSize( 1, 1, 1);  //TODO
+
+
+  const dim3 blockSize(32, 32, 1);  //TODO
+  const dim3 gridSize(std::ceil(numCols/32), std::ceil(numRows/32), 1);
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
